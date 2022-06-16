c2122b2747277ce3273001fb4ce8e28bf1fc01ce
ovi2410 <guneshwarifegade@gmail.com>
2022-06-16 14:55:33 +0530

    [ovi]refractor the code and select query based on name

--- ADOPayrollService/uc4_RefactorCode_selectquerybasedonname/EmployeeDataManager.cs
+++ ADOPayrollService/uc4_RefactorCode_selectquerybasedonname/EmployeeDataManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uc3_updatebasicpayinsqlserver
+namespace uc4_RefactorCode_selectquerybasedonname
 {
     class EmployeeDataManager
     {
