4e10f48344c02118d6e554a7b4f6af4d83116fb0
ovi2410 <guneshwarifegade@gmail.com>
2022-06-16 14:44:20 +0530

    [ovi]create database

---
+++ ADOPayrollService/uc1_createdatabase/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace ADO_Employee_Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll Services using ADO!");
+        }
+    }
+}
